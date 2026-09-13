f5dcaa8a077acd47154a1eacf53427e902024765
karriem-lang <328486186+Karriem-T-Lang@users.noreply.github.com>
2026-09-13 17:46:18 -0400

    Initial Commit

---
+++ WWDC_CreateUI_Prototypes_Agents/WWDC_CreateUI_Prototypes_AgentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC_CreateUI_Prototypes_AgentsApp.swift
+//  WWDC_CreateUI_Prototypes_Agents
+//
+//  Created by Karriem Lang on 9/13/26.
+//
+
+import SwiftUI
+
+@main
+struct WWDC_CreateUI_Prototypes_AgentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
